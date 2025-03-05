d98e8ff88efe280d090bd56275a336a5423381bd
AkaTFL <coldway3108@gmail.com>
2025-03-05 17:20:22 +0100

    avancée front

--- dapp-rpt/app/page.tsx
+++ dapp-rpt/app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { RPT_ABI } from "@/public/RollerPaperTycoon";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useReadContract, useWriteContract } from "wagmi";
+
+/**
+ * Adresse du contrat RPT ERC20.
+ * Cette adresse est utilisée pour interagir avec le contrat intelligent RPT.
+ */
+const RPT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export default function Blockchain() {
+  const { writeContract} = useWriteContract();
+  const { address, isConnected } = useAccount();
+
+  /**
+   * Lecture du solde du contrat RPT ERC20.
+   * Cette lecture permet de récupérer le solde du contrat RPT ERC20 pour l'adresse connectée.
+   * @dev refetch permet de rafraîchir le solde du contrat RPT ERC20.
+   */
+  const { data: balance, refetch } = useReadContract({
+    abi: RPT_ABI, // ABI du contrat RPT ERC20
+    functionName: "balanceOf", // Nom de la fonction à appeler
+    address: RPT_ADDRESS, // Adresse du contrat RPT ERC20
+    args: address ? [address] : undefined, // Only pass args if address exists
+  });
+
+  /**
+   * Fonction pour créer des jetons RPT ERC20.
+   * Cette fonction permet de créer des jetons RPT ERC20 pour l'adresse connectée.
+   */
+  const handleClick = () => {
+    if (!address) return;
+    writeContract({
+      abi: RPT_ABI, // ABI du contrat RPT ERC20
+      functionName: "mint", // Nom de la fonction à appeler
+      address: RPT_ADDRESS, // Adresse du contrat RPT ERC20
+      args: [address, 100000n], // Argument pour la fonction mint
+    });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+    <div className="flex flex-col items-center place-content-between" style={{ height: "calc(100vh - 64px)" }}>
+      <div className="mt-4 flex flex-col h-fit flex items-center place-content-between">
+        <ConnectButton/>
+        {isConnected ? (
+          <div className="">
+            <p>BONSOIR</p>
+          </div>
+        ) : (
+          <div className="">
+            <p className="text-lg text-gray-500">
+              Connectez-vous avec votre wallet
+            </p>
+          </div>
+        )}
+      </div>
+      {isConnected ? (
+      <div className="">
+        <p className="text-lg text-gray-500">
+          Score :{balance?.toString()}
+        </p>
+      </div> 
+      ) : (
+        <div className="">
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        )}
+      </div>
+    </>
   );
 }
